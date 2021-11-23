e79302055a8720c3651ff935ca1df78825bbd05f
Christian Asia <christianasia75@gmail.com>
2021-11-23 21:19:56 +0800

    Adding Retrieval and Local Storage

--- main.js
+++ main.js
@@ -55,3 +55,65 @@ function yes() {
   } 
 
 }
+
+
+
+
+let patient = [];
+    addPatients = (e) => {
+        e.preventDefault();
+
+        let Patients = {
+            id: Date.now(),
+            firstname: document.getElementById("fname").value,
+            middlename: document.getElementById("middlename").value,
+            lastname: document.getElementById("lname").value,
+            address: document.getElementById("address").value,
+            birthdate: document.getElementById("birthday").value,
+            mobileNumber: document.getElementById("phone").value,
+            gender: document.getElementById("male").checked == true ? document.getElementById("male").value : document.getElementById("female").value,
+            medicalHistory: [...document.querySelectorAll('input[name="symptoms"]:checked')].map((option) => option.value),
+            
+        }
+
+        patient.push(Patients);
+        console.table(Patients)
+
+        document.querySelector("#display pre").innerHTML = "\n" + JSON.stringify(Patients, null, 2);
+
+        localStorage.setItem("Patients", JSON.stringify(patient))
+
+        // document['#contact'].reset();
+    }
+
+    document.getElementById("submitBtn").addEventListener(
+        "click",  addPatients
+
+    );
+
+
+    listPatient = (e) => {
+        e.preventDefault();
+
+        let newArray = [];
+        newArray = JSON.parse(localStorage.getItem("Patients"));
+        console.table(newArray);
+
+        let listBtn = document.getElementById("listpatient");
+
+        newArray.forEach((review, i) => {
+            let listRows = listBtn.insertRow(i+1);
+            let j = 0;
+            for (let property in review) {
+                let listCols = listRows.insertCell(j);
+                listCols.innerText = review[property];
+                j++;
+            }
+        });
+    }
+
+    document.getElementById("list").addEventListener(
+        "click", listPatient
+    )
+
+
